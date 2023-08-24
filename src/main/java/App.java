3be743137828e07caf9bfb861fe85c87c4072f94
Ruslan <r.schewtschenko@mail.ru>
2023-08-24 13:33:55 +0300

    Task solved.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,23 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHelloWorld1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(beanHelloWorld1.getMessage());
+
+        HelloWorld beanHelloWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHelloWorld2.getMessage());
+
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getMessage());
+
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getMessage());
+
+        System.out.println("beanHelloWorld1 = beanHelloWorld1 -> " + beanHelloWorld1.equals(beanHelloWorld2));
+        System.out.println("beanCat1 = beanCat2 -> " + beanCat1.equals(beanCat2));
     }
 }
